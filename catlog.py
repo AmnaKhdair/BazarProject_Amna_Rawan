d42a2b09000a989782077ee280979004b6e348c3
AmnaKhdair <amenahabd2000@gmail.com>
2022-04-01 20:32:27 +0300

    Create info function

--- catlog.py
+++ catlog.py
@@ -9,7 +9,14 @@ app = Flask(__name__)
 def home():
     return "hello world"
 
-@app.route("/info/<id>",methods['GET'])
-def Info():
-    
-    return "hello world test"
+@app.route("/info/<id>",methods=['GET'])
+def info(id):
+    with open('/home/amnakhdair/Desktop/projects/BooksDB.json', 'r') as DBfile:
+        data = DBfile.read()
+        jsonObject = json.loads(data)
+        BooksRecords = jsonObject['BOOK']
+        idInt = int(id)
+        result=[BooksRecordsItem for BooksRecordsItem in BooksRecords if BooksRecordsItem['ID'] == idInt]
+        if len(result)==0:
+            return  "No such Book Found!!"
+        return jsonify([{"Numbers OF books":result[0]['NUMBERS'],"Cost":result[0]['COST']}])
