7b0f160e1bcc3cce466a4793cc1be200c7034e42
AmnaKhdair <amenahabd2000@gmail.com>
2022-04-06 01:10:43 +0300

    create info route

--- frontend.py
+++ frontend.py
@@ -9,3 +9,8 @@ app = Flask(__name__)
 def home():
     return "hello world"
 
+@app.route("/info/<id>",methods=['GET'])
+def info(id):
+    idInt=int(id)
+    result=requests.get("http://192.168.56.101:5000/info/{}".format(idInt))
+    return (result.content)
